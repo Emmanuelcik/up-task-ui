2abb4240829e515bbac491aa87a9767fb24d4766
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-09-25 22:43:15 -0600

    created initial app layout

---
+++ src/componentes/Logo.tsx
@@ -0,0 +1,5 @@
+const Logo = () => {
+  return <img src="/logo.svg" alt="Up Task App logo" />;
+};
+
+export default Logo;
---
+++ src/views/DashboardView.tsx
@@ -0,0 +1,5 @@
+const DashboardView = () => {
+  return <h1>test</h1>;
+};
+
+export default DashboardView;
