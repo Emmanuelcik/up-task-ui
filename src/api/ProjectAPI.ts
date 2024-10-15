5d42cd3a61efa86fcb0f5d83adaa12d71014c3ca
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-10-15 00:24:48 -0600

    added edition of a project

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import { dashboardProjectSchema, ProjectFormData } from "../types";
+import { dashboardProjectSchema, Project, ProjectFormData } from "../types";
 import { isAxiosError } from "axios";
 
 export const createProject = async (formData: ProjectFormData) => {
@@ -20,6 +20,40 @@ export const getProjects = async () => {
     if (response.success) {
       return response.data;
     }
+  } catch (error) {
+    console.log(error);
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message);
+    }
+  }
+};
+export const getProjectById = async (id: Project["_id"]) => {
+  try {
+    const { data } = await api(`/projects/${id}`);
+    // const response = dashboardProjectSchema.safeParse(data);
+    // if (response.success) {
+    //   return response.data;
+    // }
+    return data;
+  } catch (error) {
+    console.log(error);
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message);
+    }
+  }
+};
+
+type ProjectAPIType = {
+  formData: ProjectFormData;
+  projectId: Project["_id"];
+};
+export const updateProject = async ({
+  formData,
+  projectId,
+}: ProjectAPIType) => {
+  try {
+    const { data } = await api.put<string>(`/projects/${projectId}`, formData);
+    return data;
   } catch (error) {
     console.log(error);
     if (isAxiosError(error) && error.response) {
