7a750a6d3fad7ca00afd3d106a945007967a59cc
Emmmanuel Lopez <jesusemmcik@gmail.com>
2025-04-14 23:26:58 -0600

    restore vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
     },
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
 });
- 
